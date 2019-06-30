70a4764335e223b0ee9946e7afa4bbbbaf202050
NataBoginia <nata_boginia@mail.ru>
2019-06-30 16:46:26 +0300

    Open and close dialog with click and Enter

--- js/setup.js
+++ js/setup.js
@@ -64,7 +64,7 @@ var createWizardsFragment = function (wizards, template) {
 var wizards = createWizards(WIZARDS_QUANTITY);
 
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+// userDialog.classList.remove('hidden');
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
@@ -74,3 +74,72 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
 similarListElement.appendChild(createWizardsFragment(wizards, similarWizardTemplate));
 
 document.querySelector('.setup-similar').classList.remove('hidden');
+
+
+// Учебный проект: одеть Надежду
+/* Открытие/закрытие окна настройки персонажа:
+
+Окно .setup должно открываться по нажатию на блок .setup-open. Открытие окна производится удалением класса hidden у блока
+
+Окно .setup должно закрываться по нажатию на элемент .setup-close, расположенный внутри окна
+
+Добавить обработчики для альтернативного ввода с клавиатуры keydown для кнопок открытия/закрытия диалога настройки персонажа:
+
+Когда иконка пользователя в фокусе .setup-open-icon, то окно настройки персонажа должно открываться по нажатию кнопки ENTER.
+Не забудьте добавить  tabindex="0" для иконки пользователя, чтобы она фокусировалась;
+Когда окно настройки персонажа открыто, нажатие на клавишу ESC должно закрывать диалог.
+Если фокус находится на форме ввода имени, то окно закрываться не должно;
+Если окно открыто и фокус находится на кнопке закрытия окна, то нажатие клавиши ENTER должно приводить к закрытию диалога;
+Если диалог открыт, нажатие на кнопку «Сохранить» приводит к отправке формы;
+Если диалог открыт и фокус находится на кнопке «Сохранить», нажатие на ENTER приводит к отправке формы;
+*/
+
+var ESC_CODE = 27;
+var ENTER_CODE = 13;
+
+/**
+  * Показывает диалог персонажа, добавляет обработчик события на кнопку закрытия диалога
+  *
+  */
+var openDialog = function () {
+  userDialog.classList.remove('hidden');
+  document.addEventListener('keydown', onDialogEscPress);
+};
+
+/**
+  * Скрывает диалог персонажа, удаляет обработчик события на кнопке закрытия диалога
+  *
+  */
+var closeDialog = function () {
+  userDialog.classList.add('hidden');
+  document.removeEventListener('keydown', onDialogEscPress);
+};
+
+/**
+  * Скрывает диалог персонажа по кнопке Esc
+  *
+  */
+var onDialogEscPress = function (evt) {
+  if (evt.keyCode === ESC_CODE) {
+    userDialog.classList.add('hidden');
+  }
+};
+
+var setupOpenElement = document.querySelector('.setup-open');
+var setupOpenIcon = document.querySelector('.setup-open-icon');
+var setupCloseElement = userDialog.querySelector('.setup-close');
+
+
+setupOpenElement.addEventListener('click', function () {
+  openDialog();
+});
+
+setupCloseElement.addEventListener('click', function () {
+  closeDialog();
+});
+
+setupOpenIcon.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_CODE) {
+    openDialog();
+  }
+});
