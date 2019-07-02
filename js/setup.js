7e36f7d32bb06262622404d7849711b0c148836f
NataBoginia <nata_boginia@mail.ru>
2019-07-02 18:01:40 +0300

    Add changing colors

--- js/setup.js
+++ js/setup.js
@@ -4,7 +4,10 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var WIZARDS_QUANTITY = 4;
+var ESC_CODE = 27;
+var ENTER_CODE = 13;
 
 /**
   * Создает тестовый массив волшебников определенной длины,
@@ -61,45 +64,8 @@ var createWizardsFragment = function (wizards, template) {
   return fragment;
 };
 
-var wizards = createWizards(WIZARDS_QUANTITY);
-
-var userDialog = document.querySelector('.setup');
-// userDialog.classList.remove('hidden');
-
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
-
-similarListElement.appendChild(createWizardsFragment(wizards, similarWizardTemplate));
-
-document.querySelector('.setup-similar').classList.remove('hidden');
-
-
-// Учебный проект: одеть Надежду
-/* Открытие/закрытие окна настройки персонажа:
-
-Окно .setup должно открываться по нажатию на блок .setup-open. Открытие окна производится удалением класса hidden у блока
-
-Окно .setup должно закрываться по нажатию на элемент .setup-close, расположенный внутри окна
-
-Добавить обработчики для альтернативного ввода с клавиатуры keydown для кнопок открытия/закрытия диалога настройки персонажа:
-
-Когда иконка пользователя в фокусе .setup-open-icon, то окно настройки персонажа должно открываться по нажатию кнопки ENTER.
-Не забудьте добавить  tabindex="0" для иконки пользователя, чтобы она фокусировалась;
-Когда окно настройки персонажа открыто, нажатие на клавишу ESC должно закрывать диалог.
-Если фокус находится на форме ввода имени, то окно закрываться не должно;
-Если окно открыто и фокус находится на кнопке закрытия окна, то нажатие клавиши ENTER должно приводить к закрытию диалога;
-Если диалог открыт, нажатие на кнопку «Сохранить» приводит к отправке формы;
-Если диалог открыт и фокус находится на кнопке «Сохранить», нажатие на ENTER приводит к отправке формы;
-*/
-
-var ESC_CODE = 27;
-var ENTER_CODE = 13;
-
 /**
-  * Показывает диалог персонажа, добавляет обработчик события на кнопку закрытия диалога
-  *
+  * Показывает диалог персонажа, добавляет закрытие диалога по Esc
   */
 var openDialog = function () {
   userDialog.classList.remove('hidden');
@@ -107,8 +73,7 @@ var openDialog = function () {
 };
 
 /**
-  * Скрывает диалог персонажа, удаляет обработчик события на кнопке закрытия диалога
-  *
+  * Скрывает диалог персонажа, удаляет закрытие диалога по Esc
   */
 var closeDialog = function () {
   userDialog.classList.add('hidden');
@@ -116,30 +81,117 @@ var closeDialog = function () {
 };
 
 /**
-  * Скрывает диалог персонажа по кнопке Esc
-  *
+  * Скрывает диалог персонажа по кнопке Esc, если фокус не в поле ввода имени
+  * @param {Object} evt
   */
 var onDialogEscPress = function (evt) {
-  if (evt.keyCode === ESC_CODE) {
-    userDialog.classList.add('hidden');
+  if (evt.keyCode === ESC_CODE && evt.target !== setupNameInput) {
+    closeDialog();
   }
 };
 
+/**
+  * Меняет заливку svg-элемента случайный образом из входящего массива цветов, в инпуте возвращает значение цвета
+  * @param {Object} element - DOM-элемент, для которого надо поменять цвет
+  * @param {Object} input - DOM-элемент поля ввода, для которого нужно прописать значение цвета
+  * @param {string[]} colors - Массив возможных цветов
+  */
+
+var changeFillColor = function (element, input, colors) {
+  var color = colors[Math.floor(Math.random() * colors.length)];
+  element.style.fill = color;
+  input.value = color;
+};
+
+/**
+  * Меняет цвет фона элемента случайный образом из входящего массива цветов, в инпуте возвращает значение цвета
+  * @param {Object} element - DOM-элемент, для которого надо поменять цвет фона
+  * @param {Object} input - DOM-элемент поля ввода, для которого нужно прописать значение цвета
+  * @param {string[]} colors - Массив возможных цветов
+  */
+var changeBackgroundColor = function (element, input, colors) {
+  var color = colors[Math.floor(Math.random() * colors.length)];
+  element.style.backgroundColor = color;
+  input.value = color;
+};
+
+// генерация массива случайных волшебников
+var wizards = createWizards(WIZARDS_QUANTITY);
+
+// DOM-элемент диалога
+var userDialog = document.querySelector('.setup');
+
+// элемент для вывода случайных волшебников в диалоге
+var similarListElement = document.querySelector('.setup-similar-list');
+
+// шаблон для элемента случайного волшебника
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+// элементы, по которым открывается диалог
 var setupOpenElement = document.querySelector('.setup-open');
 var setupOpenIcon = document.querySelector('.setup-open-icon');
+
+// управляющие элементы диалога - кнопка закрытия, поле ввода имени
 var setupCloseElement = userDialog.querySelector('.setup-close');
+var setupNameInput = userDialog.querySelector('.setup-user-name');
 
+// интерактивные элементы волшебника: мантия, глаза и файербол
+var wizardCoat = userDialog.querySelector('.setup-wizard .wizard-coat');
+var wizardCoatInput = userDialog.querySelector('#coat-color');
+var wizardEyes = userDialog.querySelector('.setup-wizard .wizard-eyes');
+var wizardEyesInput = userDialog.querySelector('#eyes-color');
+var wizardFireball = userDialog.querySelector('.setup-fireball-wrap');
+var wizardFireballInput = userDialog.querySelector('#fireball-color');
 
+// добавление случайных волшебников в окно диалога
+similarListElement.appendChild(createWizardsFragment(wizards, similarWizardTemplate));
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+/**
+  * Открыть диалог по клику на элемент с иконкой пользователя
+  */
 setupOpenElement.addEventListener('click', function () {
   openDialog();
 });
 
-setupCloseElement.addEventListener('click', function () {
-  closeDialog();
-});
-
+/**
+  * Открыть диалог по нажатию Enter на иконке пользователя
+  */
 setupOpenIcon.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_CODE) {
     openDialog();
   }
+});
+
+/**
+  * Закрыть диалог по клику на крестик в окне диалога
+  */
+setupCloseElement.addEventListener('click', function () {
+  closeDialog();
+});
+
+/**
+  * Закрыть диалог по нажатию Enter на крестике в окне диалога
+  */
+setupCloseElement.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_CODE) {
+    closeDialog();
+  }
+});
+
+/**
+  * Добавить обработчики нажатий на мантию, глаза и файербол волшебника
+  */
+wizardCoat.addEventListener('click', function () {
+  changeFillColor(wizardCoat, wizardCoatInput, COAT_COLORS);
+});
+
+wizardEyes.addEventListener('click', function () {
+  changeFillColor(wizardEyes, wizardEyesInput, EYES_COLORS);
+});
+
+wizardFireball.addEventListener('click', function () {
+  changeBackgroundColor(wizardFireball, wizardFireballInput, FIREBALL_COLORS);
 });
